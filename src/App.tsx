e4ebc1aad7f5d17cd44ca3bffb781d440f136b18
w0wayush <ayupandey82@gmail.com>
2024-11-10 17:09:30 +0530

    Fixed few things

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import LogInPage from "./Pages/LogInPage";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
 import ListPage from "./Pages/ListPage";
 import ChatPage from "./Pages/ChatPage";
 import ProfilePage from "./Pages/ProfilePage";
 import Layout from "./Pages/Layout";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<LogInPage />} />
-
-        {/* Nested Routing */}
+        <Route path="/auth" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<Layout />}>
           <Route index element={<ListPage />} />
           <Route path="chat" element={<ChatPage />} />
           <Route path="profile" element={<ProfilePage />} />
         </Route>
-
         <Route path="*" element={<Navigate to="/dashboard" />} />
       </Routes>
     </BrowserRouter>
