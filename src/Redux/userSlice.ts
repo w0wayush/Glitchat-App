eb565846b0afa1f20bf49e57a7ef8e7731f71b56
w0wayush <ayupandey82@gmail.com>
2024-11-11 18:59:32 +0530

    Added task list slice and upgraded user slice

--- src/Redux/userSlice.ts
+++ src/Redux/userSlice.ts
@@ -1,7 +1,7 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { userType } from "../Types";
 
-export const userStorageName = "chat_user";
+export const userStorageName = "superhero_user";
 
 export const defaultUser: userType = {
   id: "",
@@ -13,44 +13,56 @@ export const defaultUser: userType = {
   lastSeen: "",
   bio: "",
 };
+type userStateType = {
+  users: userType[];
+  currentUser: userType;
+  alertProps: {
+    open: boolean;
+    recieverId: string;
+    recieverName: string;
+  };
+};
 
-/* 
-const initialUserData = localStorage.getItem(userStorageName);
-export const defaultUser: userType = initialUserData
-  ? JSON.parse(initialUserData)
-  : {
-      id: "",
-      username: "",
-      email: "",
-      isOnline: false,
-      img: "",
-      creationTime: "",
-      lastSeen: "",
-      bio: "",
-    };
-*/
-
-const initialState = {
-  // user: [],
+const initialState: userStateType = {
+  users: [],
   currentUser: defaultUser,
-  // currentSelectedUser: null
+  alertProps: {
+    open: false,
+    recieverId: "",
+    recieverName: "",
+  },
+  // currentSelectedUser:null
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<userType>) => {
+    setUser: (state, action) => {
       const user = action.payload;
+
+      // store user in local storage
       localStorage.setItem(userStorageName, JSON.stringify(user));
-      //set logged in user
+
+      // set loged in user
       state.currentUser = user;
     },
-    setUsers: (state, action: PayloadAction<userType>) => {
-      //set all users
+    setUsers: (state, action) => {
+      // set all users
+      state.users = action.payload;
+    },
+    setAlertProps: (state, action) => {
+      const { open, recieverId, recieverName } = action.payload;
+
+      state.alertProps = {
+        open,
+        recieverId: recieverId || "",
+        recieverName: recieverName || "",
+      };
     },
   },
 });
 
-export const { setUser, setUsers } = userSlice.actions;
+export const { setUser, setUsers, setAlertProps } = userSlice.actions;
+
 export default userSlice.reducer;
