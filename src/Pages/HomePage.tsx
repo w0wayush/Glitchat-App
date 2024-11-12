ba449e3d1cc7f4e1e3b23a03d318f35ff35680b3
Ayush Pandey <ayupandey82@gmail.com>
2024-11-13 00:44:05 +0530

    Started with main homepage

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import FeatureSection from "../Components/Homepage/FeatureSection";
+import HeroSection from "../Components/Homepage/HeroSection";
+import Navbar from "../Components/Homepage/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <FeatureSection />
+    </div>
+  );
+};
+
+export default HomePage;
