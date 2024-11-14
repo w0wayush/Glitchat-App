e57780a784625f0a32f122fc6d2369f0b77b173d
Ayush Pandey <ayupandey82@gmail.com>
2024-11-14 22:48:31 +0530

    Updated pages layout

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
+import Alert from "../Components/Alert";
 
 type Props = {};
 
-const Layout = (props: Props) => {
+function Layout({}: Props) {
   return (
-    <div className="h-[100vh] flex flex-col ">
+    <div className="h-[100vh] flex flex-col">
       <Header />
-      <div className="bg-pattern flex-1 max-h-[90%] overflow-y-scroll">
+      <div className="bg-pattern flex-1 max-h-[90%] overflow-y-scroll no-scrollbar">
         <Outlet />
       </div>
+      <Alert />
     </div>
   );
-};
+}
 
 export default Layout;
