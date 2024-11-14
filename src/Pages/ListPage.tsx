c9b5a2471d6885de4104112ea0752122e42ea5e2
Ayush Pandey <ayupandey82@gmail.com>
2024-11-14 22:49:46 +0530

    Updated list page

--- src/Pages/ListPage.tsx
+++ src/Pages/ListPage.tsx
@@ -3,10 +3,12 @@ import SingleTaskList from "../Components/SingleTaskList";
 import { BE_getTaskList } from "../Backend/Queries";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../Redux/store";
+import { ListLoader } from "../Components/Loaders";
+import FlipMove from "react-flip-move";
 
 type Props = {};
 
-const ListPage = (props: Props) => {
+function ListPage({}: Props) {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   const taskList = useSelector(
@@ -18,14 +20,22 @@ const ListPage = (props: Props) => {
   }, [dispatch]);
 
   return (
-    <div className="p-10 z-5">
-      <div className="flex flex-wrap justify-center gap-10">
-        {taskList.map((t) => (
-          <SingleTaskList singleTaskList={t} />
-        ))}
-      </div>
+    <div className="p-10">
+      {loading ? (
+        <ListLoader />
+      ) : taskList.length === 0 ? (
+        <h1 className="text-3xl text-center text-gray-500 mt-10">
+          No task list added, add some!
+        </h1>
+      ) : (
+        <FlipMove className="flex flex-wrap justify-center gap-10">
+          {taskList.map((t) => (
+            <SingleTaskList key={t.id} singleTaskList={t} />
+          ))}
+        </FlipMove>
+      )}
     </div>
   );
-};
+}
 
 export default ListPage;
